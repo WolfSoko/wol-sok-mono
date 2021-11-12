672ab8fb560d8a4ca1f708a6285007f1f6798638
Wolfram Sokollek <wolframsokollek@gmail.com>
2021-11-12 12:18:34 +0100

    initial commit

---
+++ apps/angular-examples/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wol-sok-mono-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-examples';
+}
