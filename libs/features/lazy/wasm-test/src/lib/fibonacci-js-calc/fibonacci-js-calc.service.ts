ded052ab222da772099ca5cf21c1b5875cf27e21
Wolfram Sokollek <wolframsokollek@gmail.com>
2022-04-29 21:11:15 +0200

    fix(utils-decorators): copy next to usage as workaround

--- libs/features/lazy/wasm-test/src/lib/fibonacci-js-calc/fibonacci-js-calc.service.ts
+++ libs/features/lazy/wasm-test/src/lib/fibonacci-js-calc/fibonacci-js-calc.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { Memoized } from '../../../../../../utils/decorators/src';
+import { Memoized } from './memoize-copy';
 
 @Injectable({
   providedIn: 'root',
