879bcfafe8e217c81bb82da403eb44fa96b4c361
Wolfram Sokollek <wolframsokollek@gmail.com>
2022-05-12 19:36:02 +0200

    feat: nrwl/cli migrate to 14.1.5

--- libs/utils/decorators/jest.config.ts
+++ libs/utils/decorators/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'libs/utils-decorators',
 
   globals: {
@@ -11,5 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/utils/decorators',
-  preset: '../../../jest.preset.ts',
+  preset: '../../../jest.preset.js',
 };
--- libs/utils/operators/jest.config.ts
+++ libs/utils/operators/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'utils-operators',
 
   globals: {
@@ -11,5 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/utils/operators',
-  preset: '../../../jest.preset.ts',
+  preset: '../../../jest.preset.js',
 };
