429896898c184b25a98a1357279506e86e84220c
Wolfram Sokollek <wolframsokollek@gmail.com>
2022-09-24 22:39:24 +0200

    test: gravity-world.component.ts

--- apps/feat-lazy-gravity-rocks-e2e/src/e2e/gravity-rocks/gravity-rocks.component.cy.ts
+++ apps/feat-lazy-gravity-rocks-e2e/src/e2e/gravity-rocks/gravity-rocks.component.cy.ts
@@ -4,4 +4,10 @@ describe('feat-lazy-gravity-rocks', () => {
     cy.get('feat-lazy-gravity-rocks').should('exist');
     cy.get('mat-toolbar').should('contain', 'Gravity World');
   });
+
+  it('should start and stop the simulation', () => {
+    cy.get('.configuration button').should('contain.text', 'Start').click();
+    cy.get('.configuration button').should('contain.text', 'Pause').click();
+    cy.get('.configuration button').should('contain.text', 'Start');
+  });
 });
