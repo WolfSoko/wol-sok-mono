db9afbf43eac5f9bebe76c50a0686911dd372d33
Wolfram Sokollek <wolframsokollek@gmail.com>
2022-09-25 14:27:39 +0200

    feat: integrate release version in sentry config

--- apps/angular-examples/src/main.ts
+++ apps/angular-examples/src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableAkitaProdMode } from '@datorama/akita';
 import * as Sentry from '@sentry/angular';
 import { BrowserTracing } from '@sentry/tracing';
+import { version } from '../.././../package.json';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -13,26 +14,28 @@ if (environment.production) {
 
 Sentry.init({
   dsn: 'https://d7eab9a5f3484e48b3cf9c110533dcbe@o1384048.ingest.sentry.io/6702682',
+  release: `angular-examples@${version}`,
   integrations: [
     new BrowserTracing({
       tracingOrigins: [
         'localhost',
         'https://angularexamples.wolsok.de/',
-        'https://angularexamples-b69f4.firebaseio.com'
+        'https://angularexamples-b69f4.firebaseio.com',
       ],
-      routingInstrumentation: Sentry.routingInstrumentation
-    })
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
   ],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
-  tracesSampleRate: 1.0
+  tracesSampleRate: 1.0,
 });
 
 // persistState({exclude: ['performance-test', 'wasm-test', 'input-wave', 'game-state', 'bacteria-player']});
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
 
 // platformBrowserDynamic().bootstrapModule(AppModule).then(moduleRef => {
 //   const applicationRef = moduleRef.injector.get(ApplicationRef);
