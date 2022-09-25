833f3adff5657a3f42f1b01d37f45bc8acf111ef
Wolfram Sokollek <wolframsokollek@gmail.com>
2022-09-25 15:18:31 +0200

    2.4.8

--- apps/angular-examples/src/app/app.component.ts
+++ apps/angular-examples/src/app/app.component.ts
@@ -1,25 +1,30 @@
-import {ChangeDetectionStrategy, Component, Inject} from "@angular/core";
-import {Angulartics2GoogleTagManager} from "angulartics2";
-import {MainNavRoute} from "./app-routing.module";
-import {TitleService} from "./core/title.service";
-import {ROUTER_LINKS} from "./router-links.token";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Inject,
+} from '@angular/core';
+import { Angulartics2GoogleTagManager } from 'angulartics2';
+import { environment } from '../environments/environment';
+import { MainNavRoute } from './app-routing.module';
+import { TitleService } from './core/title.service';
+import { ROUTER_LINKS } from './router-links.token';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  @HostBinding('attr.app-version') appVersionAttr = 'v' + environment.version;
 
   constructor(
     // we need title service to update page title.
     private titleService: TitleService,
     gtmManager: Angulartics2GoogleTagManager,
-    @Inject(ROUTER_LINKS) public routerLinks: MainNavRoute[]) {
+    @Inject(ROUTER_LINKS) public routerLinks: MainNavRoute[]
+  ) {
     gtmManager.startTracking();
   }
-
-
 }
-
