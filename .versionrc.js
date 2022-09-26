9fdd6ce1a447095ed9b0348584d9f38779195bc6
Wolfram Sokollek <wolframsokollek@gmail.com>
2022-09-27 00:35:20 +0200

    feat(changelog): also print CI and Refactor sections
    - ci: This should maybe also be put into Changelog
    - refactor: This should maybe also be put into Changelog

--- .versionrc.js
+++ .versionrc.js
@@ -6,4 +6,15 @@ const versionFile = {
 module.exports = {
   bumpFiles: [versionFile],
   packageFiles: [versionFile],
+  types: [
+    { type: 'feat', section: 'Features' },
+    { type: 'fix', section: 'Bug Fixes' },
+    { type: 'refactor', hidden: 'Refactorings' },
+    { type: 'ci', hidden: 'Continuous Integration' },
+    { type: 'chore', hidden: true },
+    { type: 'docs', hidden: true },
+    { type: 'style', hidden: true },
+    { type: 'perf', hidden: true },
+    { type: 'test', hidden: true },
+  ],
 };
