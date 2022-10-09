0e8bfef288f7872448e7cab454b2b22b1f42af9c
Wolfram Sokollek <wolframsokollek@gmail.com>
2022-10-09 23:13:03 +0200

    test(e2e): add cypress dashboard project d

--- apps/fourier-analysis-remote-e2e/cypress.config.ts
+++ apps/fourier-analysis-remote-e2e/cypress.config.ts
@@ -2,5 +2,6 @@ import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: 'jyisda',
   e2e: nxE2EPreset(__dirname),
 });
