d05e8799af0436ef49fd292a64d437c49ceac448
Wolfram Sokollek <wolframsokollek@gmail.com>
2022-11-30 13:21:47 +0100

    refactor(neural-network): clean up module

--- apps/angular-examples/src/app/feature/lazy/neural-network/neural-network.module.ts
+++ apps/angular-examples/src/app/feature/lazy/neural-network/neural-network.module.ts
@@ -1,28 +1,23 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import {SharedModule} from '../../../shared/shared.module';
-import {NeuralNetworkComponent} from './neural-network.component';
-import {BrainService} from './shared/brain.service';
-import {TrainDataService} from './shared/train-data.service';
-import {PerceptronComponent} from './shared/perceptron/perceptron.component';
-import {DataViewComponent} from './shared/data-view/data-view.component';
-import {MultiPerceptronComponent} from './multi-perceptron/multi-perceptron.component';
-import {PerceptronTabComponent} from './perceptron-tab/perceptron-tab.component';
-import {BrainSettingsComponent} from './shared/brain-settings/brain-settings.component';
-import {NeuralNetworkRoutingModule} from './neural-network-routing.module';
+import { SharedModule } from '../../../shared/shared.module';
+import { MultiPerceptronComponent } from './multi-perceptron/multi-perceptron.component';
+import { NeuralNetworkRoutingModule } from './neural-network-routing.module';
+import { NeuralNetworkComponent } from './neural-network.component';
+import { PerceptronTabComponent } from './perceptron-tab/perceptron-tab.component';
+import { BrainSettingsComponent } from './shared/brain-settings/brain-settings.component';
+import { DataViewComponent } from './shared/data-view/data-view.component';
+import { PerceptronComponent } from './shared/perceptron/perceptron.component';
 
 @NgModule({
-  imports: [
-    SharedModule,
-    NeuralNetworkRoutingModule
-  ],
-  declarations: [NeuralNetworkComponent,
+  imports: [SharedModule, NeuralNetworkRoutingModule],
+  declarations: [
+    NeuralNetworkComponent,
     PerceptronComponent,
     DataViewComponent,
     MultiPerceptronComponent,
     PerceptronTabComponent,
-    BrainSettingsComponent],
-  providers: [BrainService, TrainDataService]
+    BrainSettingsComponent,
+  ],
 })
-export class NeuralNetworkModule {
-}
+export class NeuralNetworkModule {}
