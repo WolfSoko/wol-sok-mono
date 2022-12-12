aac1d5f8621b16aab907c2295d00b07224ddd104
Wolfram Sokollek <wolframsokollek@gmail.com>
2022-12-12 10:34:10 +0100

    chore(renovate): update allowed post upgrade commands

--- renovate.config.js
+++ renovate.config.js
@@ -21,6 +21,7 @@ module.exports = {
     '^npx --no-install ng update (@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]* --from=\\d+\\.\\d+\\.\\d+ --to=\\d+\\.\\d+\\.\\d+ --migrate-only --allow-dirty --force$',
     '^npx --no-install ng lint --fix$',
     '^npx --no-install nx migrate (@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]* --from=(@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]*@\\d+\\.\\d+\\.\\d+ --to=(@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]*@\\d+\\.\\d+\\.\\d+$',
+    '^\\[ -f migrations\\.json \\] && npx --no-install nx migrate --run-migrations=migrations\\.json$',
     '^rm -f migrations.json || true$',
     '^npx --no-install nx workspace-lint$',
     '^npx --no-install nx run-many --target=lint --all --parallel --fix --skip-nx-cache$',
