cd3342ea3c6902a193c91e4f969034192bcdb86a
Wolfram Sokollek <wolframsokollek@gmail.com>
2023-01-12 12:01:09 +0100

    test(gravity-world): don't use spectator for now
    - until spectator issue fixes
    
    refs: https://github.com/ngneat/spectator/pull/599

--- libs/features/lazy/gravity-rocks/src/lib/gravity-rocks/gravity-world/gravity-world.component.spec.ts
+++ libs/features/lazy/gravity-rocks/src/lib/gravity-rocks/gravity-world/gravity-world.component.spec.ts
@@ -1,18 +1,40 @@
-import { createHostFactory } from '@ngneat/spectator';
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { GravityWorldComponent } from './gravity-world.component';
 
 describe('GravityWorldComponent', () => {
-  const createHost = createHostFactory(GravityWorldComponent);
+  // TODO: reactivate when error with standalone is fixed: https://github.com/ngneat/spectator/pull/599
+  // const createHost = createHostFactory(GravityWorldComponent);
 
-  beforeEach(() => {
+  let component: HostComponent;
+
+  @Component({
+    standalone: true,
+    imports: [GravityWorldComponent],
+    template: ` <feat-lazy-gravity-world></feat-lazy-gravity-world>`,
+  })
+  class HostComponent {}
+
+  beforeEach(async () => {
     global.ResizeObserver = jest.fn().mockImplementation(() => ({
       observe: jest.fn(),
       unobserve: jest.fn(),
       disconnect: jest.fn(),
     }));
+
+    await TestBed.configureTestingModule({
+      imports: [HostComponent, NoopAnimationsModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    const fixture = TestBed.createComponent(HostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the component', () => {
-    expect(createHost(`<feat-lazy-gravity-world></feat-lazy-gravity-world>`).component).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
