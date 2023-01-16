e8559bd7e3e06c36907dda10945632535b971171
Wolfram Sokollek <wolframsokollek@gmail.com>
2023-01-16 13:38:31 +0100

    test: use spectator 14 and revert temporary fix
    refs: https://github.com/nrwl/nx/releases

--- libs/features/lazy/gravity-rocks/src/lib/gravity-rocks/gravity-world/gravity-world.component.spec.ts
+++ libs/features/lazy/gravity-rocks/src/lib/gravity-rocks/gravity-world/gravity-world.component.spec.ts
@@ -1,40 +1,18 @@
-import { Component } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { createHostFactory } from '@ngneat/spectator';
 import { GravityWorldComponent } from './gravity-world.component';
 
 describe('GravityWorldComponent', () => {
-  // TODO: reactivate when error with standalone is fixed: https://github.com/ngneat/spectator/pull/599
-  // const createHost = createHostFactory(GravityWorldComponent);
+  const createHost = createHostFactory(GravityWorldComponent);
 
-  let component: HostComponent;
-
-  @Component({
-    standalone: true,
-    imports: [GravityWorldComponent],
-    template: ` <feat-lazy-gravity-world></feat-lazy-gravity-world>`,
-  })
-  class HostComponent {}
-
-  beforeEach(async () => {
+  beforeEach(() => {
     global.ResizeObserver = jest.fn().mockImplementation(() => ({
       observe: jest.fn(),
       unobserve: jest.fn(),
       disconnect: jest.fn(),
     }));
-
-    await TestBed.configureTestingModule({
-      imports: [HostComponent, NoopAnimationsModule],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    const fixture = TestBed.createComponent(HostComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create the component', () => {
-    expect(component).toBeTruthy();
+    expect(createHost(`<feat-lazy-gravity-world></feat-lazy-gravity-world>`).component).toBeTruthy();
   });
 });
--- libs/ui-kit/src/lib/let/let.directive.spec.ts
+++ libs/ui-kit/src/lib/let/let.directive.spec.ts
@@ -1,15 +1,14 @@
-import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { fakeAsync } from '@angular/core/testing';
+import { createDirectiveFactory } from '@ngneat/spectator/jest';
 import { Subject } from 'rxjs';
-import * as webpack from 'webpack';
 import { LetDirective } from './let.directive';
 
 describe('LetDirective', () => {
+  const createDirective = createDirectiveFactory(LetDirective);
+
   it('should make the "as" variable available in the context', () => {
     const spectator = createDirective(
-      `<div *wsLet='true as testReference'><span class='use-value'>{{testReference}}</span></div>`
+      `<div *wsLet="true as testReference"><span class="use-value">{{testReference}}</span></div>`
     );
     const span = spectator.query('span.use-value');
     expect(span?.textContent).toContain('true');
@@ -17,7 +16,7 @@ describe('LetDirective', () => {
 
   it(`should use let to assign the variable, renders also falsy values`, () => {
     const spectator = createDirective(
-      `<div *wsLet='false; let testReference;'><span class='use-value'>{{testReference}}</span></div>`
+      `<div *wsLet="false; let testReference;"><span class="use-value">{{testReference}}</span></div>`
     );
     const span = spectator.query('span.use-value');
     expect(span?.textContent).toContain('false');
@@ -27,8 +26,8 @@ describe('LetDirective', () => {
     const testSubject = new Subject<{ emission: number }>();
     const spectator = createDirective(
       `
-<div *wsLet='(test$ | async) as testReference'>
-  <span class='use-value'>emission:{{testReference?.emission ?? 'undefined'}}</span>
+<div *wsLet="(test$ | async) as testReference">
+  <span class="use-value">emission:{{testReference?.emission ?? 'undefined'}}</span>
 </div>
 `,
       { hostProps: { test$: testSubject.asObservable() } }
@@ -51,34 +50,4 @@ describe('LetDirective', () => {
     spectator.query('span.use-value');
     expect(span?.textContent).toContain('emission:2');
   }));
-
-  function createDirective(template: string, { hostProps }: { hostProps?: Record<string, any> } = { hostProps: {} }) {
-    @Component({
-      template,
-      standalone: true,
-      imports: [LetDirective, AsyncPipe],
-    })
-    class DirectiveHostComponent implements Record<string, any> {
-      [key: string]: any;
-
-      constructor() {
-        for (const key in hostProps) {
-          this[key] = hostProps[key];
-        }
-      }
-    }
-
-    TestBed.configureTestingModule({
-      imports: [DirectiveHostComponent],
-    }).compileComponents();
-
-    const fixture: ComponentFixture<DirectiveHostComponent> = TestBed.createComponent(DirectiveHostComponent);
-    fixture.detectChanges();
-    return {
-      fixture: fixture,
-      component: fixture.componentInstance,
-      query: (css: string) => fixture.debugElement.query(By.css(css)).nativeElement,
-      detectChanges: () => fixture.detectChanges(),
-    };
-  }
 });
