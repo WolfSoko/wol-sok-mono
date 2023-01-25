22abfa87d9606b1c9744e28b4e5e4aca882f7398
Wolfram Sokollek <wolframsokollek@gmail.com>
2023-01-25 17:15:01 +0100

    feat: enable sentry replay

--- apps/angular-examples/src/bootstrap.ts
+++ apps/angular-examples/src/bootstrap.ts
@@ -14,9 +14,19 @@ if (environment.production) {
 Sentry.init({
   dsn: 'https://d7eab9a5f3484e48b3cf9c110533dcbe@o1384048.ingest.sentry.io/6702682',
   release: `angular-examples@${environment.version}`,
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  // eslint-disable-next-line no-magic-numbers
+  replaysSessionSampleRate: 1,
+
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
   integrations: [
+    new Sentry.Replay(),
     new BrowserTracing({
-      tracingOrigins: ['https://angularexamples.wolsok.de/'],
+      tracePropagationTargets: ['https://angularexamples.wolsok.de/'],
       routingInstrumentation: Sentry.routingInstrumentation,
     }),
   ],
