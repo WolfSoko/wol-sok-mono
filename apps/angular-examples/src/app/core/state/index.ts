548f371f41b40b33e534b18c76311f204f60a90f
Wolfram Sokollek <wolframsokollek@gmail.com>
2023-11-21 18:33:16 +0100

    refactor: ♻️ remove outdated code

--- apps/angular-examples/src/app/core/state/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './shader-examples-shader-code-data.service';
-export * from './shader-examples-shader-code.query';
-export * from './shader-examples-shader-code.store';
-export * from './shader-examples-shader-code.service';
-export * from './shader-examples-shader-code.model';
