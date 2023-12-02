474c724a5f87430d07c9d3cf04c6cb36d87ab724
Wolfram Sokollek <wolframsokollek@gmail.com>
2023-12-02 10:13:44 +0100

    refactor(angular-examples): ♻️ load sentry by module

--- apps/angular-examples/src/app/core/core.module.ts
+++ apps/angular-examples/src/app/core/core.module.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
-import * as Sentry from '@sentry/angular-ivy';
+import { createErrorHandler, TraceService } from '@sentry/angular-ivy';
 import { AuthModule } from '@wolsok/feat-api-auth';
 import { provideDataAccess } from '@wolsok/shared-data-access';
 import { Angulartics2Module } from 'angulartics2';
@@ -32,19 +32,19 @@ export const provideCore: () => Array<Provider | EnvironmentProviders> = () => [
   ),
   {
     provide: ErrorHandler,
-    useValue: Sentry.createErrorHandler({
+    useValue: createErrorHandler({
       showDialog: true,
     }),
   },
   {
-    provide: Sentry.TraceService,
+    provide: TraceService,
     deps: [Router],
   },
   {
     provide: APP_INITIALIZER,
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     useFactory: () => () => {},
-    deps: [Sentry.TraceService],
+    deps: [TraceService],
     multi: true,
   },
 ];
--- apps/angular-examples/src/bootstrap.ts
+++ apps/angular-examples/src/bootstrap.ts
@@ -2,7 +2,12 @@ import { provideHttpClient } from '@angular/common/http';
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { enableAkitaProdMode } from '@datorama/akita';
-import * as Sentry from '@sentry/angular-ivy';
+import {
+  BrowserTracing,
+  init,
+  Replay,
+  routingInstrumentation,
+} from '@sentry/angular-ivy';
 import { provideWsThanosOptions } from '@wolsok/thanos';
 import { provideAppRouter } from './app/app-routing';
 import { AppComponent } from './app/app.component';
@@ -14,7 +19,7 @@ if (environment.production) {
   enableAkitaProdMode();
 }
 
-Sentry.init({
+init({
   dsn: 'https://d7eab9a5f3484e48b3cf9c110533dcbe@o1384048.ingest.sentry.io/6702682',
   release: `angular-examples@${environment.version}`,
   // This sets the sample rate to be 10%. You may want this to be 100% while
@@ -27,13 +32,12 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 
   integrations: [
-    new Sentry.Replay(),
-    new Sentry.BrowserTracing({
-      tracePropagationTargets: ['https://angularexamples.wolsok.de/'],
-      routingInstrumentation: Sentry.routingInstrumentation,
+    new Replay(),
+    new BrowserTracing({
+      routingInstrumentation,
     }),
   ],
-
+  tracePropagationTargets: ['https://angularexamples.wolsok.de/'],
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
@@ -53,17 +57,3 @@ bootstrapApplication(AppComponent, {
     ],
   ],
 }).catch((err) => console.error(err));
-
-// persistState({exclude: ['performance-test', 'wasm-test', 'input-wave', 'game-state', 'bacteria-player']});
-// platformBrowserDynamic()
-//   .bootstrapModule(AppModule, {
-//     defaultEncapsulation: ViewEncapsulation.Emulated,
-//   })
-//   .catch((err) => console.error(err));
-
-// platformBrowserDynamic().bootstrapModule(AppModule).then(moduleRef => {
-//   const applicationRef = moduleRef.injector.get(ApplicationRef);
-//   const componentRef = applicationRef.components[0];
-//   // allows to run `ng.profiler.timeChangeDetection();`
-//   enableDebugTools(componentRef);
-// }).catch(err => window['console'].error(err));
