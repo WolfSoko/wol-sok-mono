3c583628412f861b9a5f18408add6bb5cec823df
Wolfram <wolframsokollek@gmail.com>
2024-02-09 21:49:15 +0100

    test(feat-lazy-tf-examples): 🚨 use nx cypress plugin structure

--- libs/features/lazy/tensorflow-examples/cypress.config.ts
+++ libs/features/lazy/tensorflow-examples/cypress.config.ts
@@ -3,7 +3,18 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: nxE2EPreset(__filename, { cypressDir: 'cypress' }),
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'cypress',
+      webServerCommands: {
+        default: 'nx run angular-examples:serve',
+      },
+      webServerConfig: {
+        timeout: 120000,
+      },
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
   projectId: 'jyisda',
   component: nxComponentTestingPreset(__filename),
 });
