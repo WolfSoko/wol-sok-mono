86f8d05ed0b02b45c57a62cddcb6d8b95cb49a20
Wolfram <wolframsokollek@gmail.com>
2024-02-26 08:35:37 +0100

    feat(angular-examples-cdk): ✨ fix config

---
+++ apps/angular-examples-cdk/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'angular-examples-cdk',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/angular-examples-cdk',
+};
---
+++ apps/angular-examples-cdk/src/main.ts
@@ -0,0 +1,14 @@
+import { App, RemovalPolicy } from 'aws-cdk-lib';
+import { SpaStack } from './stacks/spa.stack';
+
+const app = new App();
+
+new SpaStack(app, 'AngularExamples', {
+  env: {
+    region: 'eu-central-1',
+    account: '088632064895',
+  },
+  buildOutputPath: 'dist/production',
+  domainName: 'angularexamples.wolsok.de',
+  deleteBucketPolicy: RemovalPolicy.DESTROY,
+});
