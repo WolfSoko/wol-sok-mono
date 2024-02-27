e7891c83d3b8a7518d30817b8d6ba764338c65e3
Wolfram <wolframsokollek@gmail.com>
2024-02-27 21:28:33 +0100

    chore(release): 🚀 add release to allowed scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,14 @@
-module.exports = {extends: ['@commitlint/config-nx-scopes']};
+const {
+  utils: { getProjects },
+} = require('@commitlint/config-nx-scopes');
+
+module.exports = {
+  rules: {
+    'scope-enum': async (ctx) => [
+      2,
+      'always',
+      [...(await getProjects(ctx)), 'release'],
+    ],
+  },
+  // . . .
+};
