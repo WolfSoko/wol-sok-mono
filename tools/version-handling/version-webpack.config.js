b3e64e3c987dce98c1d91124145e4ff48d542c6f
Wolfram <wolframsokollek@gmail.com>
2024-02-28 05:06:44 +0100

    ci: 👷 agent should checkout deeper to fetch tags appropriate

--- tools/version-handling/version-webpack.config.js
+++ tools/version-handling/version-webpack.config.js
@@ -1,25 +1,32 @@
+'use strict';
+
 const { execSync } = require('child_process');
 const { DefinePlugin } = require('webpack');
+
+const latestVersionTag = getLatestVersionTag();
+console.log(`Version for build: ${latestVersionTag}`);
 
 module.exports = function withVersionHandling(config, context) {
   return {
     plugins: [
       new DefinePlugin({
-        VERSION: getLatestVersionTag(),
+        VERSION: latestVersionTag,
       }),
     ],
   };
 };
-
 function getLatestVersionTag() {
   // Get latest version tag like v1.0.0 that matches the regex pattern
-  const version = execSync(
-    'git describe --tags --abbrev=0 --match "v[0-9]*.[0-9]*.[0-9]*"'
-  )
-    .toString()
-    .trim();
-  if (!version) {
-    return 'vx.x.x';
+  let version;
+  try {
+    version = execSync(
+      'git describe --tags --abbrev=0 --match "v[0-9]*.[0-9]*.[0-9]*"'
+    )
+      .toString()
+      .trim();
+    return version;
+  } catch (error) {
+    console.error('Error while getting latest version tag', error);
+    return `t${Date.now()}`;
   }
-  return version;
 }
