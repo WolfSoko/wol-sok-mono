1df77ff593ba6895ad599baf32b4389f85173ccb
Wolfram <wolframsokollek@gmail.com>
2024-02-28 04:30:37 +0100

    chore: 🔧 make angular-examples dependent on the git version tag

--- tools/version-handling/version-webpack.config.js
+++ tools/version-handling/version-webpack.config.js
@@ -1,11 +1,24 @@
-const { DefinePlugin } = require('webpack');
-const { version } = require('../../version.json');
+const { execSync } = require('child_process');
+
 module.exports = function withVersionHandling(config, context) {
   return {
     plugins: [
       new DefinePlugin({
-        VERSION: JSON.stringify(version),
+        VERSION: getLatestVersionTag(),
       }),
     ],
   };
 };
+
+function getLatestVersionTag() {
+  // Get latest version tag like v1.0.0 that matches the regex pattern
+  const version = execSync(
+    'git describe --tags --abbrev=0 --match "v[0-9]*.[0-9]*.[0-9]*"'
+  )
+    .toString()
+    .trim();
+  if (!version) {
+    return 'vx.x.x';
+  }
+  return version;
+}
