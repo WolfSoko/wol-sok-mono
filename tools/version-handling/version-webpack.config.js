a9c579e39ff3b6f8aa965ce330119d715c6a3ebd
Wolfram <wolframsokollek@gmail.com>
2024-02-28 06:13:17 +0100

    chore: 🔧  fix overriding of function

--- tools/version-handling/version-webpack.config.js
+++ tools/version-handling/version-webpack.config.js
@@ -1,13 +1,13 @@
 import { latestVersionTag } from './latest-version-tag';
 import { DefinePlugin } from 'webpack';
 
-const latestVersionTag = latestVersionTag();
-console.log(`Version for build: ${latestVersionTag}`);
+const version = latestVersionTag();
+console.log(`Version for build: ${version}`);
 
 export const withVersionHandling = (config, context) => ({
   plugins: [
     new DefinePlugin({
-      VERSION: `"${latestVersionTag}"`,
+      VERSION: `"${version}"`,
     }),
   ],
 });
