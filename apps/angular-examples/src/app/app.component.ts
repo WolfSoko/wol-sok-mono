d408de6332b62f269af7fd6584e64e2e350c31f6
Wolfram <wolframsokollek@gmail.com>
2024-03-02 17:48:04 +0100

    feat(angular-examples): ✨ load version after startup

--- apps/angular-examples/src/app/app.component.ts
+++ apps/angular-examples/src/app/app.component.ts
@@ -1,11 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import {
   ChangeDetectionStrategy,
   Component,
   HostBinding,
   Inject,
+  Signal,
 } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
 import { Angulartics2GoogleTagManager } from 'angulartics2';
+import { catchError, from, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Environment } from '../environments/environment.type';
 import { MainNavRoutes } from './app-routing';
 import { ENV_TOKEN } from './core/env.token';
@@ -24,16 +29,27 @@ import { ROUTER_LINKS } from './router-links.token';
 })
 export class AppComponent {
   @HostBinding('attr.app-version')
-  appVersionAttr: string;
+  appVersionAttr: Signal<void | undefined>;
 
   constructor(
     // we need title service to update page title.
     private titleService: TitleService,
     gtmManager: Angulartics2GoogleTagManager,
     @Inject(ROUTER_LINKS) public routerLinks: MainNavRoutes,
-    @Inject(ENV_TOKEN) private env: Environment
+    @Inject(ENV_TOKEN) private env: Environment,
+    httpClient: HttpClient
   ) {
     gtmManager.startTracking();
-    this.appVersionAttr = `angular-examples@${this.env.version}`;
+    this.appVersionAttr = toSignal(
+      httpClient.get<string>('/version').pipe(
+        catchError((err, caught) => {
+          console.log('No version found', err);
+          return of('next');
+        }),
+        map((version: string) => {
+          `angular-examples@${version}`;
+        })
+      )
+    );
   }
 }
