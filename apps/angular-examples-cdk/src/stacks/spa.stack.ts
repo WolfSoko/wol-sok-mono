774a2d8d3c6ed4652f71158f1259ae30cf44e8b8
Wolfram <wolframsokollek@gmail.com>
2024-03-02 15:29:37 +0100

    feat(angular-examples-cdk): ✨ add version.json to bucket

--- apps/angular-examples-cdk/src/stacks/spa.stack.ts
+++ apps/angular-examples-cdk/src/stacks/spa.stack.ts
@@ -1,14 +1,17 @@
 import { SpaConstruct, SpaProps } from '@wolsok/spa-cdk-stack';
 import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { Source } from 'aws-cdk-lib/aws-s3-deployment';
+import * as path from 'path';
 
 export class SpaStack extends Stack {
   constructor(parent: App, name: string, props: StackProps & SpaProps) {
     super(parent, name, props);
 
-    new SpaConstruct(this, name, {
+    const spa: SpaConstruct = new SpaConstruct(this, name, {
       domainName: props.domainName,
       buildOutputPath: props.buildOutputPath,
       bucketRemovalPolicy: props.bucketRemovalPolicy,
     });
+    spa.addExtraAssets(Source.asset('version.json'));
   }
 }
