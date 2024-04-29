da1e2dbf9323820886999068148f31f0a6460cbb
Wolfram <wolframsokollek@gmail.com>
2024-04-29 19:07:24 +0200

    ci: 👷 install xi package

--- .github/actions/install-deps/install-ubuntu-packages.sh
+++ .github/actions/install-deps/install-ubuntu-packages.sh
@@ -1,3 +1,3 @@
 echo "Install packages to compile node_modules gl with node-gyp"
 sudo apt-get update -y
-sudo apt-get install xserver-xorg-dev libxi-dev libxext-dev -y
+sudo apt-get install xserver-xorg-dev xi libxi-dev libxext-dev -y
