19e42293efebb511bbb731ba121b346809e4ba02
Wolfram <wolframsokollek@gmail.com>
2024-05-02 22:53:11 +0200

    test(rollapolla): 🚨 remove deprecated RouterTestingModule

--- apps/rollapolla/src/app/app.component.spec.ts
+++ apps/rollapolla/src/app/app.component.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, WelcomeComponent, RouterTestingModule],
+      imports: [AppComponent, WelcomeComponent],
     }).compileComponents();
   });
 
--- libs/features/lazy/tensorflow-examples/src/lib/tensorflow-examples/tensorflow-examples.component.cy.ts
+++ libs/features/lazy/tensorflow-examples/src/lib/tensorflow-examples/tensorflow-examples.component.cy.ts
@@ -1,11 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { TensorflowExamplesComponent } from './tensorflow-examples.component';
 
 describe(TensorflowExamplesComponent.name, () => {
   beforeEach(() => {
     TestBed.overrideComponent(TensorflowExamplesComponent, {
-      add: { imports: [RouterTestingModule] },
+      add: { imports: [] },
     });
   });
 
