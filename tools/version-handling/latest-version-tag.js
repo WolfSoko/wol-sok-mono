d53492421d50a497d74a0bd5197f635a985e698b
Wolfram <wolframsokollek@gmail.com>
2024-05-03 22:33:31 +0200

    feat: ✨ provide local next version for development

--- tools/version-handling/latest-version-tag.js
+++ tools/version-handling/latest-version-tag.js
@@ -3,11 +3,16 @@ const { execSync } = require('node:child_process');
 module.exports = function latestVersionTag() {
   // Get latest version tag like v1.0.0 that matches the glob pattern
   try {
-    return execSync(
-      'git describe --tags --abbrev=0 --match "v[0-9]*.[0-9]*.[0-9]*" --exclude "*-deployed"'
+    const latestVersionTag = execSync(
+      'git describe --tags --abbrev=0 --match "v[0-9]*.[0-9]*.[0-9]*"'
     )
       .toString()
       .trim();
+    if (!latestVersionTag.includes('deployed')) {
+      return latestVersionTag;
+    }
+    const [major, minor, patch] = latestVersionTag.split('-')[0].split('.');
+    return `${major}.${minor}.${parseInt(patch) + 1}-next`;
   } catch (error) {
     console.error(
       'Error while getting latest version tag. Returning commit hash',
