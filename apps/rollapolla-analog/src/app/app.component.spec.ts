9800cd3f804724142db2dd29887bd838fd54d52a
Wolfram <wolframsokollek@gmail.com>
2024-05-04 00:20:42 +0200

    feat: ✨ test analogjs

---
+++ apps/rollapolla-analog/src/app/app.component.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterModule, AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
---
+++ apps/rollapolla-analog/vite.config.ts
@@ -0,0 +1,56 @@
+/// <reference types="vitest" />
+
+import analog from '@analogjs/platform';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    root: __dirname,
+    publicDir: 'src/public',
+    cacheDir: `../../node_modules/.vite`,
+
+    ssr: {
+      noExternal: ['@analogjs/trpc', '@trpc/server'],
+    },
+
+    build: {
+      outDir: '../../dist/apps/rollapolla-analog/client',
+      reportCompressedSize: true,
+      target: ['es2020'],
+    },
+    server: {
+      fs: {
+        allow: ['.'],
+      },
+    },
+    plugins: [
+      analog({
+        vite: {
+          inlineStylesExtension: 'scss',
+        },
+        nitro: {
+          routeRules: {
+            '/': {
+              prerender: false,
+            },
+          },
+        },
+      }),
+
+      nxViteTsPaths(),
+      splitVendorChunkPlugin(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      reporters: ['default'],
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
+});
