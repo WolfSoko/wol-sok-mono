5f5d716654751e074fb4bb08a9f215db2afb5216
Wolfram <wolframsokollek@gmail.com>
2024-05-12 14:04:33 +0200

    refactor: ♻️ move logic to adapter service

---
+++ apps/rollapolla-analog/src/app/adapter/polll.adapter.ts
@@ -0,0 +1,73 @@
+import { waitFor } from '@analogjs/trpc';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, NgZone, PLATFORM_ID, Signal } from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { NgForm } from '@angular/forms';
+import {
+  interval,
+  map,
+  merge,
+  shareReplay,
+  Subject,
+  switchMap,
+  take,
+} from 'rxjs';
+import { Note } from '../../note';
+import { injectTrpcClient } from '../../trpc-client';
+
+const UPDATE_INTERVAL_MS = 5000;
+
+export const pollAdapter = () => {
+  const trpc = injectTrpcClient();
+  const triggerRefresh$ = new Subject<void>();
+  const optimisticUpdates$ = new Subject<Note[]>();
+
+  const notes$ = triggerRefresh$.pipe(
+    switchMap(() => merge(optimisticUpdates$, trpc.note.list.query())),
+    map((res) => res.toReversed()),
+    shareReplay(1)
+  );
+
+  const platformId = inject(PLATFORM_ID);
+  void waitFor(notes$);
+  triggerRefresh$.next();
+  const ngZone: NgZone = inject(NgZone);
+  if (isPlatformBrowser(platformId)) {
+    ngZone.runOutsideAngular(() =>
+      interval(UPDATE_INTERVAL_MS)
+        .pipe(takeUntilDestroyed())
+        .subscribe(() => ngZone.run(() => triggerRefresh$.next()))
+    );
+  }
+
+  return {
+    notes: toSignal(notes$),
+    noteTrackBy: (index: number, note: Note) => {
+      return note.id;
+    },
+
+    addNote(newNote: string) {
+      // optimistic update
+      optimisticUpdates$.next([
+        ...(this.notes()?.toReversed() ?? []),
+        {
+          id: -1,
+          note: `(saving...) ${newNote}`,
+          createdAt: new Date().toISOString(),
+        },
+      ]);
+
+      trpc.note.create
+        .mutate({ note: newNote })
+        .pipe(take(1))
+        .subscribe(() => triggerRefresh$.next());
+    },
+
+    removeNote(id: number) {
+      trpc.note.remove
+        .mutate({ id })
+        .pipe(take(1))
+        .subscribe(() => triggerRefresh$.next());
+    },
+  };
+};
--- apps/rollapolla-analog/src/app/pages/rollapolla-welcome.component.ts
+++ apps/rollapolla-analog/src/app/pages/rollapolla-welcome.component.ts
@@ -1,21 +1,8 @@
-import { waitFor } from '@analogjs/trpc';
-import { AsyncPipe, DatePipe, isPlatformBrowser } from '@angular/common';
-import { Component, inject, NgZone, PLATFORM_ID, Signal } from '@angular/core';
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { Component, Signal } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import {
-  interval,
-  map,
-  merge,
-  shareReplay,
-  Subject,
-  switchMap,
-  take,
-} from 'rxjs';
 import { Note } from '../../note';
-import { injectTrpcClient } from '../../trpc-client';
-
-const UPDATE_INTERVAL_MS = 5000;
+import { pollAdapter } from '../adapter/polll.adapter';
 
 @Component({
   selector: 'rap-welcome',
@@ -25,67 +12,23 @@ const UPDATE_INTERVAL_MS = 5000;
   templateUrl: './rollapolla-welcome.component.html',
 })
 export class RollapollaWelcomeComponent {
-  private trpc = injectTrpcClient();
+  newNote = '';
+  private pollAdapter = pollAdapter();
+  notes: Signal<Note[] | undefined> = this.pollAdapter.notes;
+  noteTrackBy = this.pollAdapter.noteTrackBy;
 
-  private triggerRefresh$ = new Subject<void>();
-  private optimisticUpdates$ = new Subject<Note[]>();
-  private notes$ = this.triggerRefresh$.pipe(
-    switchMap(() =>
-      merge(this.optimisticUpdates$, this.trpc.note.list.query())
-    ),
-    map((res) => res.toReversed()),
-    shareReplay(1)
-  );
-
-  public notes: Signal<Note[] | undefined> = toSignal(this.notes$);
-  public newNote = '';
-
-  constructor() {
-    const platformId = inject(PLATFORM_ID);
-    void waitFor(this.notes$);
-    this.triggerRefresh$.next();
-    const ngZone: NgZone = inject(NgZone);
-    if (isPlatformBrowser(platformId)) {
-      ngZone.runOutsideAngular(() =>
-        interval(UPDATE_INTERVAL_MS)
-          .pipe(takeUntilDestroyed())
-          .subscribe(() => ngZone.run(() => this.triggerRefresh$.next()))
-      );
-    }
+  removeNote(id: number) {
+    this.pollAdapter.removeNote(id);
   }
-
-  public noteTrackBy = (index: number, note: Note) => {
-    return note.id;
-  };
-
-  public addNote(form: NgForm) {
+  addNote(form: NgForm) {
     if (!form.valid) {
       form.form.markAllAsTouched();
       return;
     }
 
     // optimistic update
-    this.optimisticUpdates$.next([
-      ...(this.notes()?.toReversed() ?? []),
-      {
-        id: -1,
-        note: `(saving...) ${this.newNote}`,
-        createdAt: new Date().toISOString(),
-      },
-    ]);
-
-    this.trpc.note.create
-      .mutate({ note: this.newNote })
-      .pipe(take(1))
-      .subscribe(() => this.triggerRefresh$.next());
+    this.pollAdapter.addNote(this.newNote);
     this.newNote = '';
     form.form.reset();
-  }
-
-  public removeNote(id: number) {
-    this.trpc.note.remove
-      .mutate({ id })
-      .pipe(take(1))
-      .subscribe(() => this.triggerRefresh$.next());
   }
 }
