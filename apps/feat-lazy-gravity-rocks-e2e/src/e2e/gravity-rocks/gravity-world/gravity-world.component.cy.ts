f5d8c8e624ce6a888ee94343dafba6ce5be3bf35
Wolfram <wolframsokollek@gmail.com>
2024-05-25 16:44:28 +0200

    test: 🚨 remove cypress storybook e2e tests

--- apps/feat-lazy-gravity-rocks-e2e/src/e2e/gravity-rocks/gravity-world/gravity-world.component.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('feat-lazy-gravity-rocks', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=gravityworldcomponent--primary'));
-
-  it('should render the component', () => {
-    cy.get('feat-lazy-gravity-world').should('exist');
-  });
-});
