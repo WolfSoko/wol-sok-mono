92a9551d25c7d06a538f0280c8b1f1ff2b658793
Wolfram <wolframsokollek@gmail.com>
2024-06-02 17:54:07 +0200

    feat(rollapolla-analog): ✨ add manifest, icons
    - better styling

--- apps/rollapolla-analog/src/app/app.component.ts
+++ apps/rollapolla-analog/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'rap-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: ` <router-outlet></router-outlet> `,
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
   constructor() {
