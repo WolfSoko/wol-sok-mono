bab0fcfe53423e9c31beeccef1325c6f689c9b32
Wolfram <wolframsokollek@gmail.com>
2024-06-15 18:23:58 +0200

    ci: 👷 use different module resolution for cdks

--- apps/rollapolla-analog/src/app/adapter/notes-repo.adapter.ts
+++ apps/rollapolla-analog/src/app/adapter/notes-repo.adapter.ts
@@ -3,7 +3,7 @@ import { toSignal } from '@angular/core/rxjs-interop';
 
 import { Timestamp } from '@angular/fire/firestore';
 import { DatabaseService, Repo } from '@wolsok/shared-data-access';
-import { map } from 'rxjs';
+import { catchError, map } from 'rxjs';
 import { Note } from '../../shared/note';
 import { NotesRepoPort } from '../ports/notes-repo.port';
 
@@ -33,7 +33,7 @@ export class NotesRepoAdapter extends NotesRepoPort {
         )
       )
     );
-    this.notes = toSignal(notes$);
+    this.notes = toSignal(notes$, { rejectErrors: true });
   }
 
   getNotes(): Signal<Note[] | undefined> {
