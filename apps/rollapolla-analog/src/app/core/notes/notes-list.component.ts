c51b55e1c67bfbb970190b9cb038a0f0f094e38f
Wolfram <wolframsokollek@gmail.com>
2024-06-16 11:47:16 +0200

    feat(rollapolla-analog): ✨ enable material typography

--- apps/rollapolla-analog/src/app/core/notes/notes-list.component.ts
+++ apps/rollapolla-analog/src/app/core/notes/notes-list.component.ts
@@ -1,5 +1,6 @@
 import { DatePipe } from '@angular/common';
-import { Component, input, output, OutputEmitterRef } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { MatCard, MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { Note } from '../../../shared/note';
 
@@ -8,16 +9,23 @@ import { Note } from '../../../shared/note';
   template: `@if (notes()) {
       <mat-list class="notes-list" data-testid="notes-list">
         @for (note of notes(); track note.id; let i = $index) {
-          <mat-list-item>
-            <span matListItemTitle data-testid="note-note">{{
-              note.note
-            }}</span>
-            <span matListItemLine>{{ note.createdAt | date: 'medium' }}</span>
-          </mat-list-item>
+          <li>
+            <mat-card>
+              <mat-card-subtitle>
+                <span data-testid="note-note">{{ note.note }}</span>
+              </mat-card-subtitle>
+              <mat-card-footer>
+                <span class="mat-label-small">{{
+                  note.createdAt | date: 'medium'
+                }}</span>
+              </mat-card-footer>
+            </mat-card>
+            <br />
+          </li>
         }
         @if (notes()?.length === 0) {
           <div>
-            <h4 class="mat-title-small">No notes yet!</h4>
+            <h4>No notes yet!</h4>
             <p>Add a new one and see them appear here...</p>
           </div>
         }
@@ -33,9 +41,11 @@ import { Note } from '../../../shared/note';
       max-height: 480px;
       overflow-y: auto;
     }
+    .notes-list {
+    }
   `,
   standalone: true,
-  imports: [DatePipe, MatListModule],
+  imports: [DatePipe, MatListModule, MatCardModule],
 })
 export class NotesListComponent {
   notes = input<Note[]>();
