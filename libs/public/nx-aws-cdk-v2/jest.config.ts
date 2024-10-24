feff2648a482e816d556078bae8f3c81b78e44c7
Wolfram <wolframsokollek@gmail.com>
2024-10-24 22:00:03 +0200

    test: remove redundant transforms

--- libs/public/nx-aws-cdk-v2/jest.config.ts
+++ libs/public/nx-aws-cdk-v2/jest.config.ts
@@ -4,9 +4,5 @@ export default {
   displayName: 'aws-cdk-v2',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/aws-cdk-v2',
 } as JestConfigWithTsJest;
