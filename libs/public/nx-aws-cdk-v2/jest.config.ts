ef03e8f981222ebd1c9c62c832700ecac3d0fc41
Wolfram <wolframsokollek@gmail.com>
2024-10-24 22:23:17 +0200

    test: use structured display name

--- libs/public/nx-aws-cdk-v2/jest.config.ts
+++ libs/public/nx-aws-cdk-v2/jest.config.ts
@@ -1,7 +1,7 @@
 import { JestConfigWithTsJest } from 'ts-jest';
 
 export default {
-  displayName: 'aws-cdk-v2',
+  displayName: { name: 'aws-cdk-v2', color: 'blue' },
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   coverageDirectory: '../../coverage/packages/aws-cdk-v2',
