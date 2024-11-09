8cf97658c0eebaedc4e19982c5aa55b2d49c6d6e
Wolfram <wolframsokollek@gmail.com>
2024-11-09 10:54:07 +0100

    fix: 🐛 remove static remotes definitions

--- apps/angular-examples/module-federation.config.ts
+++ apps/angular-examples/module-federation.config.ts
@@ -1,11 +1,7 @@
 import { ModuleFederationConfig } from '@nx/webpack';
 const config: ModuleFederationConfig = {
   name: 'angular-examples',
-  remotes: [
-    'bacteria-game-remote',
-    'fourier-analysis-remote',
-    'shader-examples-remote',
-  ],
+  remotes: [],
 };
 
 export default config;
--- apps/angular-examples/module-federation.prod.config.ts
+++ apps/angular-examples/module-federation.prod.config.ts
@@ -2,10 +2,6 @@ import { ModuleFederationConfig } from '@nx/webpack';
 
 const config: ModuleFederationConfig = {
   name: 'angular-examples',
-  remotes: [
-    ['bacteria-game-remote', 'https://bacteria-game.wolsok.de'],
-    ['fourier-analysis-remote', 'https://fourier-analysis.wolsok.de'],
-    ['shader-examples-remote', 'https://shader-examples.wolsok.de'],
-  ],
+  remotes: [],
 };
 export default config;
