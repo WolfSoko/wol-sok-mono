6155b135ef228ba781489c34ac53bbf3930db4d6
Wolfram <wolframsokollek@gmail.com>
2024-11-19 02:26:13 +0100

    test(shared-data-access): 🚨 switched to vite

--- libs/shared/data-access/src/lib/data-access/database.service.spec.ts
+++ libs/shared/data-access/src/lib/data-access/database.service.spec.ts
@@ -1,41 +1,45 @@
 import { TestBed } from '@angular/core/testing';
 import { collection, Firestore } from '@angular/fire/firestore';
-import { SpectatorService } from '@ngneat/spectator';
-import { createServiceFactory } from '@ngneat/spectator/jest';
+import { MockedObject } from '@storybook/test';
+import { vi } from 'vitest';
 
 import { DatabaseService } from './database.service';
 import { Repo } from './repo.service';
 
-jest.mock('@angular/fire/firestore', () => ({
-  ...jest.requireActual('@angular/fire/firestore'),
-  collection: jest.fn(),
+vi.mock('@angular/fire/firestore', async () => ({
+  ...(await vi.importActual('@angular/fire/firestore')),
+  collection: vi.fn(),
 }));
 describe('DatabaseService', () => {
-  let spectator: SpectatorService<DatabaseService>;
-  const createService = createServiceFactory({
-    service: DatabaseService,
-    providers: [],
-    entryComponents: [],
-    mocks: [Firestore],
+  let firestoreMock: MockedObject<Firestore>;
+  let service: DatabaseService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        DatabaseService,
+        { provide: Firestore, useValue: firestoreMock },
+      ],
+    });
+    service = TestBed.inject(DatabaseService);
   });
 
-  beforeEach(() => (spectator = createService()));
-
   it('should be created', () => {
-    expect(spectator.service).toBeTruthy();
+    expect(service).toBeTruthy();
   });
 
   describe('createRepo method', () => {
     it('should call firebase collection', () => {
-      spectator.service.createRepo('testPath');
+      service.createRepo('testPath');
       const firestore = TestBed.inject(Firestore);
-      expect(collection as unknown).toHaveBeenCalledWith(firestore, 'testPath');
+      expect(collection).toHaveBeenCalledWith(firestore, 'testPath');
     });
 
     it('should return the repo', () => {
-      const colRefResult = { test: 'test123' };
-      (collection as jest.Mock).mockReturnValue(colRefResult);
-      const repo: Repo<unknown> = spectator.service.createRepo('testPath');
+      const colRefResult = {
+        test: 'test123',
+      } as never;
+      vi.mocked(collection).mockReturnValue(colRefResult);
+      const repo: Repo<unknown> = service.createRepo('testPath');
       expect(repo).toBeInstanceOf(Repo);
     });
   });
---
+++ libs/shared/data-access/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../../node_modules/.vite/libs/shared/data-access',
+
+  plugins: [nxViteTsPaths()],
+  test: {
+    globals: true,
+    setupFiles: ['src/test-setup.ts'],
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../../coverage/libs/shared/data-access',
+      provider: 'v8',
+    },
+  },
+});
