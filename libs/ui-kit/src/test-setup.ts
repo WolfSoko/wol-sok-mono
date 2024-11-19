35c5107f5f3dbad3748afc144f6472c26e7d500e
Wolfram <wolframsokollek@gmail.com>
2024-11-19 04:01:58 +0100

    test: 🚨 fixed jest-preset-angular setup

--- libs/ui-kit/src/test-setup.ts
+++ libs/ui-kit/src/test-setup.ts
@@ -1,3 +1,6 @@
 /// <reference types="jest"/>
-import 'jest-preset-angular/setup-jest';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+setupZoneTestEnv();
+
 import 'jest-canvas-mock';
