5ca775761c58aacff6f9e76b50e28d35b356b300
Wolfram <wolframsokollek@gmail.com>
2024-12-03 03:42:18 +0100

    feat(pacetrainer): ✨ better logic and animation for count-down-circle

--- apps/pacetrainer/src/app/shared/ui/count-down-circle/count-down-circle.component.ts
+++ apps/pacetrainer/src/app/shared/ui/count-down-circle/count-down-circle.component.ts
@@ -1,8 +1,6 @@
-import { Component, computed, inject, Injector, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { Milliseconds } from '../../model/constants/time-utils';
 import { TrainingTimePipe } from '../training-time.pipe';
-
-const FULL_DASHARRAY = 283;
 
 @Component({
   selector: 'pacetrainer-countdown-circle',
@@ -12,16 +10,56 @@ const FULL_DASHARRAY = 283;
   imports: [TrainingTimePipe],
 })
 export class CountDownCircleComponent {
-  private readonly injector = inject(Injector);
-
   timeLeft = input.required<Milliseconds>(); // milliseconds
   duration = input.required<Milliseconds>(); // milliseconds
 
-  dasharray = computed(() => {
-    const timeLeft: number = this.timeLeft?.() ?? this.duration();
+  svgArc = computed(() => {
+    const timeLeft: number = this.timeLeft() ?? this.duration();
     const rawFraction = timeLeft / this.duration();
     const fraction = rawFraction - (1 / this.duration()) * (1 - rawFraction);
-    const remaining = Math.round(fraction * FULL_DASHARRAY);
-    return `${remaining} ${FULL_DASHARRAY}`;
+    return describeArc(50, 50, 45, -90, 360 * fraction - 90);
   });
+}
+
+function polarToCartesian(
+  centerX: number,
+  centerY: number,
+  radius: number,
+  angleInDegrees: number
+): { x: number; y: number } {
+  const angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+
+  return {
+    x: centerX + radius * Math.cos(angleInRadians),
+    y: centerY + radius * Math.sin(angleInRadians),
+  };
+}
+
+function describeArc(
+  x: number,
+  y: number,
+  radius: number,
+  startAngle: number,
+  endAngle: number
+): string {
+  const start = polarToCartesian(x, y, radius, endAngle);
+  const end = polarToCartesian(x, y, radius, startAngle);
+
+  const largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+
+  const d = [
+    'M',
+    start.x,
+    start.y,
+    'A',
+    radius,
+    radius,
+    0,
+    largeArcFlag,
+    0,
+    end.x,
+    end.y,
+  ].join(' ');
+
+  return d;
 }
