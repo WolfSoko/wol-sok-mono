e3d879389e13161e36a7914da20b5cf7ba3abc68
Wolfram <wolframsokollek@gmail.com>
2024-12-08 08:22:27 +0100

    style(rollapolla-analog): 🎨 optimize imports

--- apps/rollapolla-analog/src/app/pages/rollapolla-welcome.component.ts
+++ apps/rollapolla-analog/src/app/pages/rollapolla-welcome.component.ts
@@ -1,9 +1,7 @@
-import { AsyncPipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormField } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
@@ -13,10 +11,7 @@ import { NotesComponent } from '../core/notes/notes.component';
 @Component({
   selector: 'rap-welcome',
   imports: [
-    AsyncPipe,
     FormsModule,
-    DatePipe,
-    MatFormField,
     MatInputModule,
     MatIconModule,
     MatButtonModule,
