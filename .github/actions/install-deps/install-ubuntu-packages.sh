796371091e4d8a2e43d093b7bf55087ca562707a
Wolfram <wolframsokollek@gmail.com>
2025-03-12 10:27:01 +0100

    chore: 🔧 add GTK and Graphene dependencies for node-gyp compilation on Ubuntu

--- .github/actions/install-deps/install-ubuntu-packages.sh
+++ .github/actions/install-deps/install-ubuntu-packages.sh
@@ -1,3 +1,3 @@
 echo "Install packages to compile node_modules gl with node-gyp"
 sudo apt-get update -y
-sudo apt-get install xserver-xorg-dev libxi-dev libxext-dev -y
+sudo apt-get install xserver-xorg-dev libxi-dev libxext-dev libgtk-4.so.1 libgraphene-1.0.so.0 -y
