41862d8d5ce5045f591fa6fc1a1c9a6117ab9e05
Wolfram <wolframsokollek@gmail.com>
2025-03-31 08:22:47 +0200

    build(angular-examples-cdk): 📦️ update tsconfig to use bundler and target ESNext
    - Change `moduleResolution` to "bundler" to fix cannot find file errors
    
    refactor(feat-lazy-tf-examples): update cypress config to use fileURLToPath
    
     for compatibility with ES modules.

--- libs/features/lazy/tensorflow-examples/cypress.config.ts
+++ libs/features/lazy/tensorflow-examples/cypress.config.ts
@@ -1,10 +1,14 @@
 import { nxComponentTestingPreset } from '@nx/angular/plugins/component-testing';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
+import { fileURLToPath } from 'node:url';
+
+// Convert import.meta.url to a file path
+const filename = fileURLToPath(import.meta.url);
 
 export default defineConfig({
   e2e: {
-    ...nxE2EPreset(__filename, {
+    ...nxE2EPreset(filename, {
       cypressDir: 'cypress',
       webServerCommands: {
         default: 'nx run angular-examples:serve',
@@ -16,5 +20,5 @@ export default defineConfig({
     baseUrl: 'http://localhost:4200',
   },
   projectId: 'jyisda',
-  component: nxComponentTestingPreset(__filename),
+  component: nxComponentTestingPreset(filename),
 });
