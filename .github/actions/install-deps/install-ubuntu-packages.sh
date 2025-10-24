a76d35f90b8ca9d7edf7cfb6959e09c6ff4012d3
Wolfram <wolframsokollek@gmail.com>
2025-10-24 06:46:03 +0200

    ci: 👷 update package installation to include mesa-common-dev

--- .github/actions/install-deps/install-ubuntu-packages.sh
+++ .github/actions/install-deps/install-ubuntu-packages.sh
@@ -1,3 +1,3 @@
 echo "Install packages to compile node_modules gl with node-gyp"
 sudo apt-get update -y
-sudo apt-get install xserver-xorg-dev libxi-dev libxext-dev libgtk-4-1 libgraphene-1.0-dev -y
+sudo apt-get install xserver-xorg-dev libxi-dev mesa-common-dev libgtk-4-1 libgraphene-1.0-dev -y
