e8b7d1ebd8e81b70303e37d4a427cb65bc258413
Wolfram <wolframsokollek@gmail.com>
2025-10-24 09:08:34 +0200

    ci: 👷 update CI checks to use nx-cloud for format and sync checks

--- apps/bacteria-game-remote/playwright.config.ts
+++ apps/bacteria-game-remote/playwright.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig, devices } from '@playwright/test';
-import { nxE2EPreset } from '@nx/playwright/preset';
-
 import { workspaceRoot } from '@nx/devkit';
+import { nxE2EPreset } from '@nx/playwright/preset';
+import { defineConfig, devices } from '@playwright/test';
 
 // For CI, you may want to set BASE_URL to the deployed application.
 const baseURL = process.env['BASE_URL'] || 'http://localhost:4202';
@@ -10,7 +9,7 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:4202';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  ...nxE2EPreset(import.meta.url, { testDir: './e2e' }),
+  ...nxE2EPreset(__filename, { testDir: './e2e' }),
   timeout: 120_000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
