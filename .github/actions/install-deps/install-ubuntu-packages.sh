6734dedbfa1590e529330be0b1ae5cd5369dd576
WolfSoko <wolframsokollek@gmail.com>
2025-11-03 17:47:38 +0100

    fix: 🐛 update install script to include x11 package for node-gyp compilation

--- .github/actions/install-deps/install-ubuntu-packages.sh
+++ .github/actions/install-deps/install-ubuntu-packages.sh
@@ -1,3 +1,3 @@
 echo "Install packages to compile node_modules gl with node-gyp"
 sudo apt-get update -y
-sudo apt-get install python3 python-is-python3 make gcc g++ xserver-xorg-dev libxi-dev mesa-common-dev libxext-dev libgtk-4-1 libgraphene-1.0-dev -y
+sudo apt-get install python3 python-is-python3 x11 make gcc g++ xserver-xorg-dev libxi-dev mesa-common-dev libxext-dev libgtk-4-1 libgraphene-1.0-dev -y
